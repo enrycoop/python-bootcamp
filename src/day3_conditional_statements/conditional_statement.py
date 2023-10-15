dc543dcbdb35113c5042bcf999ff3e3cdc7b1c61
enrico <enrycoop92@gmail.com>
2023-10-15 08:54:04 +0200

    Day3

---
+++ src/day3_conditional_statements/conditional_statement.py
@@ -0,0 +1,14 @@
+# if contition:
+#     do this
+# else:
+#     do this
+print('Welcome to the rollercoaster!')
+height = int(input("What is your height in cm? "))
+
+# operators
+# >, <, >=, <=, !=, ==
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to graw taller before you can ride.")
