725664548c437e452c28edd97ed6b3d4d0ef5a8c
Enrico Coppolecchia <enrycoop92@gmail.com>
2023-11-17 20:16:19 +0100

    day16

---
+++ intermediate/day16_OOP/oop.py
@@ -0,0 +1,22 @@
+"""
+This is a simple script to create a turtle object.
+See https://docs.python.org/3/library/turtle.html
+"""
+
+
+from turtle import Turtle, Screen
+
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.fd(100)
+
+
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
