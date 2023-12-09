ce377ed5a668a717ff3749d28a2652f573168f24
enricocoppolecchia <enriocoppolecchia@outlook.com>
2023-12-09 15:45:42 +0100

    day26

---
+++ code/intermediate/day26_list_and_DictionaryComprehensions/list_comprehensions.py
@@ -0,0 +1,8 @@
+# new_list = [new_item for item in list]
+# Python sequences
+
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
+
+name = "Enrico"
+letter_list = [letter.upper() for letter in name]
